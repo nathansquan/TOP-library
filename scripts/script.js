ad1ada91c68ce9c3542e908e7c7ad86b28a1e908
Nathan Quan <nathansquan@gmail.com>
2023-03-18 16:56:39 -0400

    Create displayBooks() function
    - Purpose: To display each on book the page
    - Displays each book in its own div

--- scripts/script.js
+++ scripts/script.js
@@ -14,3 +14,26 @@ Book.prototype.info = function() {
 function addBookToLibrary(book) {
     myLibrary.push(book);
 }
+
+function displayBooks() {
+    myLibrary.forEach(book => {
+        // create book card
+        const card = document.createElement('div');
+        card.textContent = book.info();
+        container.appendChild(card);
+    }); 
+}
+
+// sample data
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkein', 295, 'not read yet');
+const theHobbit2 = new Book('The Hobbit2', 'J.R.R. Tolkein', 295, 'not read yet');
+const theHobbit3 = new Book('The Hobbit3', 'J.R.R. Tolkein', 295, 'not read yet');
+
+addBookToLibrary(theHobbit);
+addBookToLibrary(theHobbit2);
+addBookToLibrary(theHobbit3);
+
+
+const container = document.querySelector('.container');
+
+displayBooks();
